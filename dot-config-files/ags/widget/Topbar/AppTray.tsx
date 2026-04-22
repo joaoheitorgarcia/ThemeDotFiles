be1a3175b370c4d69133a64a50cdfd0a764c46f9
joaoheitorgarcia <71498088+joaoheitorgarcia@users.noreply.github.com>
2026-04-22 16:14:19 +0100

    fixed appTray so actions on app menus work

--- dot-config-files/ags/widget/Topbar/AppTray.tsx
+++ dot-config-files/ags/widget/Topbar/AppTray.tsx
@@ -10,23 +10,37 @@ function trayItems() {
   return Array.from(tray.items ?? []) as TrayItem[]
 }
 
-function showTrayMenu(item: TrayItem, button: Gtk.Button) {
-  const menuModel = item.menu_model ?? item.menuModel ?? item.get_menu_model?.()
-  const actionGroup = item.action_group ?? item.actionGroup ?? item.get_action_group?.()
+function trayMenuModel(item: TrayItem) {
+  return item.menu_model ?? item.menuModel ?? item.get_menu_model?.() ?? null
+}
 
-  if (!menuModel || !actionGroup) {
+function trayActionGroup(item: TrayItem) {
+  return item.action_group ?? item.actionGroup ?? item.get_action_group?.() ?? null
+}
+
+function prepareTrayMenu(item: TrayItem, button: Gtk.MenuButton) {
+  item.about_to_show?.()
+
+  const menuModel = trayMenuModel(item)
+  const actionGroup = trayActionGroup(item)
+  button.insert_action_group("dbusmenu", actionGroup)
+  button.menuModel = menuModel
+}
+
+function showTrayMenu(item: TrayItem, button: Gtk.MenuButton) {
+  prepareTrayMenu(item, button)
+
+  if (!trayMenuModel(item) || !trayActionGroup(item)) {
     item.secondary_activate?.(0, 0)
     return
   }
 
-  item.about_to_show?.()
-  button.insert_action_group("dbusmenu", actionGroup)
+  button.popup()
+}
 
-  const popover = Gtk.PopoverMenu.new_from_model(menuModel)
-  popover.hasArrow = false
-  popover.set_parent(button)
-  popover.connect("closed", () => popover.unparent())
-  popover.popup()
+function suppressTrayHighlight(button: Gtk.MenuButton) {
+  button.remove_css_class("trayPressed")
+  button.add_css_class("trayActionTaken")
 }
 
 function TrayIcon({ item }: { item: TrayItem }) {
@@ -47,31 +61,30 @@ function TrayIcon({ item }: { item: TrayItem }) {
 }
 
 function TrayButton({ item }: { item: TrayItem }) {
-  let button: Gtk.Button | undefined
-  let releasedButton = 0
-
-  function isMenuItem() {
-    return Boolean(item.is_menu ?? item.isMenu ?? item.get_is_menu?.())
-  }
-
-  function activate(button: Gtk.Button) {
-    if (isMenuItem()) {
-      showTrayMenu(item, button)
-      return
-    }
-
-    item.activate?.(0, 0)
-  }
-
   return (
-    <button
+    <menubutton
       class="appTrayItem"
+      alwaysShowArrow={false}
+      direction={Gtk.ArrowType.NONE}
+      hasFrame={false}
+      menuModel={createBinding(item, "menu-model")}
       tooltipText={createBinding(item, "tooltip-text").as((value) =>
         String(value || item.title || ""),
       )}
       $={(widget) => {
-        button = widget
         widget.set_cursor_from_name("pointer")
+        widget.insert_action_group("dbusmenu", trayActionGroup(item))
+        item.connect?.("notify::action-group", () => {
+          widget.insert_action_group("dbusmenu", trayActionGroup(item))
+        })
+        item.connect?.("notify::menu-model", () => {
+          widget.menuModel = trayMenuModel(item)
+        })
+        widget.connect("notify::active", () => {
+          if (!widget.active) {
+            suppressTrayHighlight(widget)
+          }
+        })
 
         let pressedButton = 0
 
@@ -79,11 +92,12 @@ function TrayButton({ item }: { item: TrayItem }) {
         click.set_button(0)
         click.connect("pressed", () => {
           pressedButton = click.get_current_button()
+          prepareTrayMenu(item, widget)
+          widget.remove_css_class("trayActionTaken")
           widget.add_css_class("trayPressed")
         })
         click.connect("released", () => {
           widget.remove_css_class("trayPressed")
-          releasedButton = pressedButton
 
           if (pressedButton === 3) {
             showTrayMenu(item, widget)
@@ -92,17 +106,16 @@ function TrayButton({ item }: { item: TrayItem }) {
           pressedButton = 0
         })
         widget.add_controller(click)
-      }}
-      onClicked={() => {
-        if (button && releasedButton !== 3) {
-          activate(button)
-        }
 
-        releasedButton = 0
+        const motion = Gtk.EventControllerMotion.new()
+        motion.connect("leave", () => {
+          widget.remove_css_class("trayActionTaken")
+        })
+        widget.add_controller(motion)
       }}
     >
       <TrayIcon item={item} />
-    </button>
+    </menubutton>
   )
 }
 
